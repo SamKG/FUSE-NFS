2a0244528733ba3be3a48fda6657ba2d8455a6eb
Samyak K. Gupta <skg85@ilab2.cs.rutgers.edu>
2018-12-06 15:55:55 -0500

    Update

--- clientSNFS/client.c
+++ clientSNFS/client.c
@@ -16,33 +16,33 @@ gcc -Wall client.c `pkg-config fuse --cflags --libs` -o client
 #include <errno.h>
 #include <fcntl.h>
 //We need to write these header functions (that implement network calls to server)
-#include <networkfunc.h>
+#include "networkfunc.h"
 
 static const char *client_str = "Hello World!\n";
 static const char *client_path = "/client";
-static const char *server_ip = NULL;
+static const networkInfo* netinfo = NULL;
 
 static int client_getattr(const char *path, struct stat *stbuf)
 {
 
-	return network_getattr(server_ip,path,stbuf);
+	return network_getattr(netinfo,path,stbuf);
 }
 
 static int client_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		off_t offset, struct fuse_file_info *fi)
 {
-	return network_readdir(server_ip, path, buf, filler, offset, fi);
+	return network_readdir(netinfo, path, buf, filler, offset, fi);
 }
 
 static int client_open(const char *path, struct fuse_file_info *fi)
 {
-	return network_open(server_ip, path, fi);
+	return network_open(netinfo, path, fi);
 }
 
 static int client_read(const char *path, char *buf, size_t size, off_t offset,
 		struct fuse_file_info *fi)
 {
-	return network_read(server_ip,path,buf,size,offset,fi);
+	return network_read(netinfo,path,buf,size,offset,fi);
 }
 
 static struct fuse_operations client_oper = {
--- clientSNFS/networkfunc.h
+++ clientSNFS/networkfunc.h
@@ -3,11 +3,15 @@
 
 // The job of the networkInfo struct is to store relevant data about the server (ie: address and port)
 typedef struct networkInfo {
-	int port,
-	char* address
+	int port;
+	char* address;
 } networkInfo;
 
 
 
 // Implement network functions below
 //
+//
+//
+
+int get_socket(struct networkInfo*);
