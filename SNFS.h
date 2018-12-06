c6f76d6e27ae1228731077e06ccc8e14472b9463
Michael Wu <michael@DESKTOP-LG85CF2.localdomain>
2018-12-06 16:06:59 -0500

    Added network function in networkunc.h

--- SNFS.h
+++ SNFS.h
@@ -3,4 +3,18 @@
 
 #define PORT 16555
 
+// define enums
+#define CREATE      1
+#define OPEN        2
+#define FLUSH       3
+#define RELEASE     4
+#define TRUNCATE    5
+#define GETATTR     6
+#define READ        7
+#define WRITE       8
+#define OPENDIR     9
+#define READDIR     10
+#define RELEASEDIR  11
+#define MKDIR       12
+
 #endif
--- clientSNFS/networkfunc.h
+++ clientSNFS/networkfunc.h
@@ -10,4 +10,30 @@ typedef struct networkInfo {
 
 
 // Implement network functions below
-//
+
+// sets up socket connection, returns socket descriptor
+int connection_setup(networkInfo* info){
+	// define variables
+    int sock;                         // socket descriptor for network socket
+    int err;                          // error status
+    struct sockaddr_in server_addr;   // address of SNFS server
+    
+    // create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0){
+        printf("Error creating client socket, error%d\n", errno);
+    }
+
+    // specify server address for socket
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(info->port);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	// connect to server
+    err = connect(sock, (struct sockaddr*) &server_addr, sizeof(server_addr));
+    if(connect < 0){
+        printf("Error connecting to server, error%d\n", errno);
+    }
+
+	return sock;
+}
